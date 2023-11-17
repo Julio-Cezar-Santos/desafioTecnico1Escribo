954d69313260f8fd2311a136323cf7fc0c92d3ba
Julio Cezar Trajano dos Santos <jcezar0123@hotmail.com>
2023-11-17 19:51:58 -0300

    fix: ajuste para so aceitar numeros inteiros maiores que zero

--- src/controllers/sumDivByTreeOrFive.js
+++ src/controllers/sumDivByTreeOrFive.js
@@ -1,14 +1,23 @@
 const sumDivByThreeOrFive = (req, res) => {
-    const number = Number(req.params.number);
-    let sum = 0;
-
-    for (let i = 1; i < number; i++) {
-        if (i % 3 === 0 || i % 5 === 0) {
-            sum += i;
+    try {
+        const num = Number(req.params.number);
+        console.log(typeof (num))
+        if (isNaN(num) || !Number.isInteger(num) || num <= 0) {
+            return res.status(400).json({ mesagem: 'Por favor, insira um número inteiro maior que zero.' })
         }
-    }
 
-    return res.status(200).json(`O Somatório de todos os valores inteiros divisíveis por 3 ou 5 é: ${sum}.`);
+        let sum = 0;
+
+        for (let i = 1; i < num; i++) {
+            if (i % 3 === 0 || i % 5 === 0) {
+                sum += i;
+            }
+        }
+
+        return res.status(200).json(`O Somatório de todos os valores inteiros divisíveis por 3 ou 5 é: ${sum}.`);
+    } catch (error) {
+        return res.status(500).json({ mesagem: error.message });
+    }
 };
 
 module.exports = sumDivByThreeOrFive
